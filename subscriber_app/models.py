0e932f2aba8c425d40aac4d6bf4f0dc623ac2d7f
kolmus <lukasz.kolmus@gmail.com>
2022-01-13 18:55:37 +0100

    add models

--- subscriber_app/models.py
+++ subscriber_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Subscriber(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    email = models.CharField(max_length=128, unique=True)
+    gdpr_consent = models.BooleanField()
+
+
+class SubscriberSMS(models.Model):
+
+    create_date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=16, unique=True)
+    gdpr_consent = models.BooleanField()
+
+
+class Client(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=16, unique=True)
+    email = models.CharField(max_length=128, unique=False)
+
+
+class Users(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=16)
+    email = models.CharField(max_length=128)
+    gdpr_consent = models.BooleanField()
+
