3ae1ed8991d47d1ace9369d9aa4a4e23d9cbc2f1
kolmus <lukasz.kolmus@gmail.com>
2022-01-15 00:10:19 +0100

    all users saves in 1 db connect

--- subscriber_app/management/commands/subscribers_users.py
+++ subscriber_app/management/commands/subscribers_users.py
@@ -27,29 +27,25 @@ class Command(BaseCommand):
         for client in clients:
             if client.phone in clients_phone:
                 # save client id, client phone in csv
-                with open("subscriber_app/csv/client_conflicts.csv", mode="w") as extensions_file:
+                with open("subscriber_app/csv/client_conflicts.csv", mode="a") as extensions_file:
                     extension_writer = csv.writer(extensions_file, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
                     extension_writer.writerow([client.id, client.email])
                 clients_ext_phones.append(client.phone)
             else:
                 clients_phone.append(client.phone)
 
-        clients_to_migrate = []
-        subscribers_to_migrate = []
-        sms_subscribers_to_migrate = []
+        migrated_users = []
 
         # Subscriber migration to Users
         for subscriber in subscribers:
             if subscriber.email in user_emails:
                 continue  # to change in bonus task
-
-            elif clients.filter(email=subscriber.email).exists:  # client.email = subscriber.email
+            elif clients.filter(email=subscriber.email).exists():  # client.email = subscriber.email
                 client = clients.filter(email=subscriber.email).first()
-
                 if not users.filter(phone=client.phone).exclude(email=client.email).exists():  # and not(user.phone = client.phone and user.email != client.email)
                     # make new user based on client
                     if client.phone not in clients_ext_phones:
-                        clients_to_migrate.append(
+                        migrated_users.append(
                             Users(
                                 phone=client.phone,
                                 email=client.email,
@@ -59,26 +55,26 @@ class Command(BaseCommand):
 
                 elif users.filter(phone=client.phone).exclude(email=client.email).exists():
                     # save id and email in subscriber_conflicts.csv
-                    with open("subscriber_app/csv/subscriber_conflicts.csv", mode="w") as extensions_file:
+                    with open("subscriber_app/csv/subscriber_conflicts.csv", mode="a") as extensions_file:
                         extension_writer = csv.writer(extensions_file, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
                         extension_writer.writerow([subscriber.id, subscriber.email])
 
             else:
                 # make user without phone, based on subscriber
-                subscribers_to_migrate.append(Users(email=subscriber.email, gdpr_consent=subscriber.gdpr_consent))
+                migrated_users.append(Users(email=subscriber.email, gdpr_consent=subscriber.gdpr_consent))
 
         # SubscriberSMS migration to Users
         for smssubscriber in sms_subscribers:
             if smssubscriber.phone in user_phones:
                 continue  # to change in bonus task
 
-            elif clients.filter(phone=smssubscriber.phone).exists:  # client.phone = smssubscriber.phone
+            elif clients.filter(phone=smssubscriber.phone).exists():  # client.phone = smssubscriber.phone
                 client = clients.filter(phone=smssubscriber.phone).first()
 
                 if not users.filter(email=client.email).exclude(phone=client.phone).exists():  # and not(user.email = client.email and user.phone != client.phone)
                     # make new user based on client
                     if client.phone not in clients_ext_phones:
-                        clients_to_migrate.append(
+                        migrated_users.append(
                             Users(
                                 email=client.email,
                                 phone=client.phone,
@@ -88,27 +84,17 @@ class Command(BaseCommand):
 
                 elif users.filter(email=client.email).exclude(phone=client.phone).exists():
                     # save id and phone in subscriber_sms_conflicts.csv
-                    with open("subscriber_app/csv/subscriber_sms_conflicts.csv", mode="w") as extensions_file:
+                    with open("subscriber_app/csv/subscriber_sms_conflicts.csv", mode="a") as extensions_file:
                         extension_writer = csv.writer(extensions_file, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
                         extension_writer.writerow([smssubscriber.id, smssubscriber.phone])
 
             else:
-                # make user without fffff, based on subscriber
-                subscribers_to_migrate.append(Users(phone=smssubscriber.phone, gdpr_consent=smssubscriber.gdpr_consent))
+                # make user without email, based on subscriber
+                migrated_users.append(Users(phone=smssubscriber.phone, gdpr_consent=smssubscriber.gdpr_consent))
 
         # Save in database
-        if clients_to_migrate:
-            new_cl_users = Users.objects.bulk_create(clients_to_migrate, batch_size=None)  # batch_size to change if needed
-            print(f"Added {len(new_cl_users)} new_cl_users")
+        if migrated_users:
+            new_users = Users.objects.bulk_create(migrated_users, batch_size=None)  # batch_size to change if needed
+            print(f"Added {len(new_users)} new_users")
         else:
-            print("There is no clients_to_migrate this time")
-        if subscribers_to_migrate:
-            new_sub_users = Users.objects.bulk_create(subscribers_to_migrate, batch_size=None)  # batch_size to change if needed
-            print(f"Added {len(new_sub_users)} new_sub_users")
-        else:
-            print("There is no subscribers_to_migrate this time")
-        if sms_subscribers_to_migrate:
-            new_subsms_users = Users.objects.bulk_create(sms_subscribers_to_migrate, batch_size=None)  # batch_size to change if needed
-            print(f"Added {len(new_subsms_users)} new_subsms_users")
-        else:
-            print("There is no sms_subscribers_to_migrate this time")
+            print("There is no users to migrate this time")
